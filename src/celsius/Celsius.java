f479d6762d6e224f1234e387847ad5afdc611cfd
IoannisAsimakopoulos <john@192.168.181.1>
2021-03-15 19:39:01 +0200

    new commit

---
+++ src/celsius/Celsius.java
@@ -0,0 +1,10 @@
+
+package celsius;
+public class Celsius {
+
+    
+    public static void main(String[] args) {
+        new ConvertCelsiusToFahrenheit().setVisible(true); // enable the main form 
+    }
+    
+}
